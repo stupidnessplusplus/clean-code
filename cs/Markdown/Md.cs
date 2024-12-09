a5965b09c2272daaa262a3c3d914e9eb369672ad
stupidnessplusplus <58785209+stupidnessplusplus@users.noreply.github.com>
2024-12-09 20:32:10 +0500

    Add Md implementation

--- cs/Markdown/Md.cs
+++ cs/Markdown/Md.cs
@@ -1,41 +1,222 @@
-﻿using Markdown.MdTags;
+﻿using Markdown.MdTagHandlers;
+using Markdown.MdTags;
 using Markdown.MdTags.Interfaces;
+using System.Text;
 
 namespace Markdown;
 
 public class Md : IStringProcessor
 {
-    /// <summary>
-    /// Теги в порядке, в котором происходит проверка на них.
-    /// </summary>
-    private readonly IMdTag[] _mdTags =
-    [
-        new OrderedListItemMdTag(),
-        new UnorderedListItemMdTag(),
-        new HeaderMdTag(),
-        new ImageMdTag(),
-        new LinkMdTag(),
-        new BoldTextMdTag(),
-        new ItalicTextMdTag(),
-    ];
+    private readonly ITagPairsFinder _tagPairsFinder;
+    private readonly IPairTagsIntersectionHandler _pairTagsIntersectionHandler;
+    private readonly IGroupTagInsertionsFinder _groupTagInsertionsFinder;
+    private readonly IMdTag[] _mdTags;
+
+    public Md() : this(
+        new TagPairsFinder(),
+        new PairTagsIntersectionHandler(),
+        new GroupTagInsertionsFinder(),
+        [
+            new UnderscoresBetweenNumbersIgnoreTag(),
+            new EscapeMdTag(),
+            new OrderedListItemMdTag(),
+            new UnorderedListItemMdTag(),
+            new HeaderMdTag(),
+            new ImageMdTag(),
+            new LinkMdTag(),
+            new BoldTextMdTag(),
+            new ItalicTextMdTag(),
+        ])
+    {
+    }
+
+    internal Md(
+        ITagPairsFinder tagPairsFinder,
+        IPairTagsIntersectionHandler pairTagsIntersectionHandler,
+        IGroupTagInsertionsFinder groupTagInsertionsFinder,
+        params IMdTag[] tags)
+    {
+        ArgumentNullException.ThrowIfNull(tagPairsFinder);
+        ArgumentNullException.ThrowIfNull(pairTagsIntersectionHandler);
+        ArgumentNullException.ThrowIfNull(groupTagInsertionsFinder);
+
+        _tagPairsFinder = tagPairsFinder;
+        _pairTagsIntersectionHandler = pairTagsIntersectionHandler;
+        _groupTagInsertionsFinder = groupTagInsertionsFinder;
+
+        tags ??= [];
+
+        foreach (var tag in tags)
+        {
+            ArgumentNullException.ThrowIfNull(tag);
+
+            if (!IsTagWithSupportedType(tag))
+            {
+                throw new ArgumentException($"Tag '{tag.GetType()}' is unsupported.");
+            }
+        }
+
+        _mdTags = tags;
+    }
 
     public string Render(string mdString)
     {
-        var tagsIndices = _mdTags
-            .ToDictionary(tag => tag, tag => GetTagIndices(mdString, tag).ToArray());
+        ArgumentNullException.ThrowIfNull(mdString);
 
-        throw new NotImplementedException();
+        var tagsIndices = GetTagsIndices(mdString, _mdTags)
+            .GroupBy(kv => kv.Key.TagType)
+            .ToDictionary(group => group.Key, group => group.ToDictionary());
+        var htmlReplacements = new Dictionary<int, SubstringReplacement>();
+
+        if (tagsIndices.TryGetValue(TagType.Normal, out var normalTagsIndices))
+        {
+            AddReplacementsRange(htmlReplacements, GetNormalTagsReplacements(mdString, normalTagsIndices));
+        }
+
+        if (tagsIndices.TryGetValue(TagType.Pair, out var pairTagsIndices))
+        {
+            AddReplacementsRange(htmlReplacements, GetPairTagsReplacements(mdString, pairTagsIndices));
+        }
+
+        if (tagsIndices.TryGetValue(TagType.FullLine, out var fullLineTagsIndices))
+        {
+            AddReplacementsRange(htmlReplacements, GetFullLineTagsReplacements(mdString, fullLineTagsIndices));
+        }
+
+        return GetHtmlString(mdString, htmlReplacements);
     }
 
-    private IEnumerable<int> GetTagIndices(string mdString, IMdTag tag)
+    private bool IsTagWithSupportedType(IMdTag tag)
     {
+        return tag.TagType switch
+        {
+            TagType.Normal => tag is IHtmlConverter and not IGroupedMdTag,
+            TagType.Pair => tag is IHtmlTagsPair,
+            TagType.FullLine => tag is IHtmlTagsPair,
+            TagType.Ignore => true,
+            _ => false,
+        };
+    }
+
+    private Dictionary<IMdTag, List<SubstringReplacement>> GetTagsIndices(string mdString, IEnumerable<IMdTag> tags)
+    {
+        var tagsIndices = tags.ToDictionary(tag => tag, tag => new List<SubstringReplacement>());
+
         for (var i = 0; i < mdString.Length; i++)
         {
-            if (tag.IsMdTag(mdString, i, out var tagLength))
+            foreach (var tag in tags)
             {
-                yield return i;
-                i += tagLength - 1;
+                if (tag.IsMdTag(mdString, i, out var tagLength))
+                {
+                    if (tag.TagType != TagType.Ignore)
+                    {
+                        tagsIndices[tag].Add(new SubstringReplacement(i, tagLength));
+                    }
+
+                    i += tagLength - 1;
+                    break;
+                }
             }
         }
+
+        return tagsIndices;
+    }
+
+    private void AddReplacementsRange(
+        Dictionary<int, SubstringReplacement> replacements,
+        IEnumerable<SubstringReplacement> replacementsToAdd)
+    {
+        foreach (var newReplacement in replacementsToAdd)
+        {
+            if (replacements.TryGetValue(newReplacement.Index, out var replacement))
+            {
+                var replacementsConcat = replacement.Replacement!.Contains('/')
+                    ? replacement.Replacement + newReplacement.Replacement
+                    : newReplacement.Replacement + replacement.Replacement;
+                replacement = replacement with { Replacement = replacementsConcat };
+            }
+            else
+            {
+                replacement = newReplacement;
+            }
+
+            replacements[newReplacement.Index] = replacement;
+        }
+    }
+
+    private IEnumerable<SubstringReplacement> GetNormalTagsReplacements(
+        string mdString,
+        Dictionary<IMdTag, List<SubstringReplacement>> normalTagsPositions)
+    {
+        return normalTagsPositions
+            .SelectMany(kv => kv.Value
+                .Select(x => x with { Replacement = ((IHtmlConverter)kv.Key).GetHtmlString(mdString, x.Index) }));
+    }
+
+    private IEnumerable<SubstringReplacement> GetPairTagsReplacements(
+        string mdString,
+        Dictionary<IMdTag, List<SubstringReplacement>> pairTagsPositions)
+    {
+        var pairedTags = pairTagsPositions
+            .ToDictionary(kv => kv.Key, kv => _tagPairsFinder.PairTags(mdString, kv.Value).ToList());
+        var tagPairs = _pairTagsIntersectionHandler
+            .RemoveIntersections(pairedTags)
+            .ToDictionary(kv => (IHtmlTagsPair)kv.Key, KeyValuePair => KeyValuePair.Value.AsEnumerable());
+
+        return GetPairedTagsReplacements(mdString, tagPairs);
+    }
+
+    private IEnumerable<SubstringReplacement> GetFullLineTagsReplacements(
+        string mdString,
+        Dictionary<IMdTag, List<SubstringReplacement>> fullLineTagsPositions)
+    {
+        var pairedTags = fullLineTagsPositions
+            .ToDictionary(kv => (IHtmlTagsPair)kv.Key, kv => _tagPairsFinder.PairFullLineTags(mdString, kv.Value));
+
+        return GetPairedTagsReplacements(mdString, pairedTags);
+    }
+
+    private IEnumerable<SubstringReplacement> GetPairedTagsReplacements(
+        string mdString,
+        Dictionary<IHtmlTagsPair, IEnumerable<TagReplacementsPair>> pairedTags)
+    {
+        var htmlReplacements = pairedTags
+            .SelectMany(kv => kv.Value
+                .SelectMany(pair => pair.GetHtmlReplacements(kv.Key).Flatten()));
+        var groupTagsHtmlInsertions = pairedTags
+            .Where(kv => kv.Key is IGroupedMdTag)
+            .SelectMany(kv => _groupTagInsertionsFinder
+                .GetGroupTagReplacements(mdString, ((IGroupedMdTag)kv.Key).GroupHtmlTagsPair, kv.Value));
+
+        return htmlReplacements
+            .Concat(groupTagsHtmlInsertions);
+    }
+
+    private string GetHtmlString(string mdString, Dictionary<int, SubstringReplacement> htmlReplacements)
+    {
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < mdString.Length; i++)
+        {
+            var hasReplacement = htmlReplacements.TryGetValue(i, out var replacement);
+
+            if (hasReplacement)
+            {
+                sb.Append(replacement.Replacement);
+                i += Math.Max(0, replacement.Length - 1);
+            }
+
+            if (!hasReplacement || hasReplacement && replacement.Length == 0)
+            {
+                sb.Append(mdString[i]);
+            }
+        }
+
+        if (htmlReplacements.TryGetValue(mdString.Length, out var lastReplacement))
+        {
+            sb.Append(lastReplacement.Replacement);
+        }
+
+        return sb.ToString();
     }
 }
